106df680c134ad29383e3fdc334ecf7961df4fbb
JungHoon <greenjm1@nate.com>
2023-07-07 22:28:10 +0900

    todo list user에 연결

--- todos/models.py
+++ todos/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     detail = models.TextField()
-    email = models.ForeignKey("users.User", on_delete=models.CASCADE, null = True, related_name= 'publisher')
+    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE, null = True, related_name= 'user_id')
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    
     
     def __str__(self):
         return self.title
--- todos/serializers.py
+++ todos/serializers.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'detail', 'email', 'create_time', 'update_time')
+        fields = ('id', 'title', 'detail', 'user_id', 'create_time', 'update_time')
