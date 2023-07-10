22f6c06a17a7aa6e3addd35e965e252da8806858
JungHoon <greenjm1@nate.com>
2023-07-10 17:52:16 +0900

    사용자 테이블 암호하 및 유효성 검사 추가

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ from .validators import *
 class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100, validators=[email_validate])
-    pw = models.CharField(max_length=50, validators=[password_validate])
-    phone = models.CharField(max_length=15, validators=[phone_number_validate] )
+    pw = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15, validators=[phone_number_validate])
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+import bcrypt
+
 from .serializers import UserSerializer
 from .models import User
+from .validators import password_validate
 
 # Create your views here.
 
@@ -26,12 +30,30 @@ class UserViewSet(APIView):
     POST /todo
     """
     def post(self, request, **kwargs):
+        #접근 url 제어
         if kwargs.get('id') is not None:
             return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+        
+        #등록된 id인지 확인
+        if User.objects.filter(email = request.data['email']).exists():
+            return Response("already sign up email", status=status.HTTP_400_BAD_REQUEST)
+        
+        #비밀번호 유효성 검사
+        try:
+            password_validate(request.data['pw'])
+        except:
+            return Response("wrong password", status=status.HTTP_400_BAD_REQUEST)
+        
+        data = request.data.copy() #request.data는 QueryDict 데이터로 immutable해서 mutable하게 바꿔야함
+        
+        #password 암호화
+        password = request.data['pw'].encode('utf-8')
+        password_crypt = bcrypt.hashpw(password, bcrypt.gensalt())
+        data['pw'] = password_crypt.decode('utf-8')
+        
+        user_serializer = UserSerializer(data = data)
+        if(user_serializer.is_valid()):
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
         else:
-            user_serializer = UserSerializer(data = request.data)
-            if(user_serializer.is_valid()):
-                user_serializer.save()
-                return Response(user_serializer.data, status=status.HTTP_200_OK)
-            else:
-                return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
