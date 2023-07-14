d82a1bedc40f6ca37066fd3e5dcf7685087c36ff
JungHoon <greenjm1@nate.com>
2023-07-15 00:36:36 +0900

    auth키 blacklist 등록 기능 구현

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.cache import cache
 
 import bcrypt
 import jwt
+from jwt.exceptions import ExpiredSignatureError
 from todo_api.settings import SECRET_KEY
 
 from .serializers import UserSerializer
@@ -11,7 +13,7 @@ from .models import User
 from .validators import password_validate
 
 # Login view
-# /user/login
+# /users/auth
 class UserLoginViewSet(APIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -29,14 +31,38 @@ class UserLoginViewSet(APIView):
         if not bcrypt.checkpw(pw_encode, user_object.pw.encode('utf-8')):
             return Response("wrong password try again", status=status.HTTP_400_BAD_REQUEST)
         
-        data = jwt.encode({'user_id' : user_object.id, 
-                    'user_email' : user_object.email}, SECRET_KEY, algorithm='HS256')
+        data = jwt.encode({'user_id' : user_object.id,
+                    'user_email' : user_object.email,
+                    'user_name' : user_object.name,
+                    "exp" : int(time.time()) + (60*60*3),
+                    }, SECRET_KEY, algorithm='HS256')
         
         return Response(data, status=status.HTTP_200_OK)
-
+    
+    def delete(self, requests, **kwargs):
+        print(requests.META)
+        
+        if('HTTP_AUTHORIZATION' not in requests.META):
+            return Response("please append header your auth key", status=status.HTTP_500_BAD_REQUEST)
+        
+        auth = requests.META.get('HTTP_AUTHORIZATION')
+        
+        if(tokenEffectCheck(auth)):
+            return Response("wrong token", status=status.HTTP_500_BAD_REQUEST)
+        
+        cache.set(auth, True, 60*60*3)
+        
+        return Response("로그아웃", status=status.HTTP_200_OK)
+    
+    def tokenEffectCheck(token):
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except ExpiredSignatureError:
+            return False
+        return True
 
 # user basic view
-# /user or /user/<int:id>
+# /users or /users/<int:id>
 class UserViewSet(APIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
