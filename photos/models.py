10eba1f7a1e9a07a7bbb97f1ef2fcd3fd12e1dde
JungHoon <greenjm1@nate.com>
2023-07-18 16:23:35 +0900

    model 오류 코드 수정

--- photos/models.py
+++ photos/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Todo(models.Model):
+class Photo(models.Model):
     title = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="images/")
-    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE, null = True, related_name= 'user_id')
+    image = models.ImageField(upload_to="images")
+    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE, null = True, related_name= 'photo_user_id')
     update_time = models.DateTimeField(auto_now=True)
     
     def __str__(self):
