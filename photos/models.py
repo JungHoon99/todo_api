01c857d8595c06ffffe8cccb4d874dd460f50050
JungHoon <greenjm1@nate.com>
2023-07-18 15:12:22 +0900

    models define

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="images/")
+    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE, null = True, related_name= 'user_id')
+    update_time = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
