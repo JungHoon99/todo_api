4e22cdd4216147516c787bc119a7447de56dac11
JungHoon <greenjm1@nate.com>
2023-07-18 15:07:50 +0900

    photos create

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from todos.views import TodoViewSet
 from users.views import UserViewSet
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/', include('todos.urls')),
     path('users/', include('users.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
